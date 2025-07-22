3b88ba4425669956303bd30ca39394c7166c6309
Ahmettcakar <116648416+Ahmettcakar@users.noreply.github.com>
2025-07-22 17:04:02 +0300

    yeni modeller

--- Models/KaynakDongusu.cs
+++ Models/KaynakDongusu.cs
@@ -11,28 +11,20 @@ namespace FanucRelease.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
-        [Display(Name = "Başlangıç Saati")]
         [DataType(DataType.Time)]
         public TimeOnly BaslangicSaati { get; set; }
 
-        [Display(Name = "Bitiş Saati")]
         [DataType(DataType.Time)]
         public TimeOnly? BitisSaati { get; set; }
 
-        [Display(Name = "Toplam Kaynak Zamanı (saniye)")]
         public double? ToplamSureSaniye { get; set; }
 
-        [Display(Name = "Döngü Tamamlandı")]
         public bool Tamamlandi { get; set; } = false;
 
-        [Display(Name = "Oluşturulma Tarihi")]
         [DataType(DataType.Date)]
         public DateTime OlusturulmaTarihi { get; set; } = DateTime.Today;
 
         // Foreign Key
-        [Required]
-        [Display(Name = "Operatör")]
         public int OperatorId { get; set; }
 
         // Navigation Property
@@ -40,7 +32,6 @@ namespace FanucRelease.Models
         public virtual Operator Operator { get; set; } = null!;
 
         // Hesaplanmış özellik - Dakika cinsinden süre
-        [Display(Name = "Toplam Süre (dakika)")]
         public double? ToplamSureDakika => ToplamSureSaniye.HasValue ? ToplamSureSaniye.Value / 60 : null;
     }
 }
--- Models/MakineDurus.cs
+++ Models/MakineDurus.cs
@@ -11,19 +11,13 @@ namespace FanucRelease.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
-        [Display(Name = "Duruş Başlangıç Saati")]
         [DataType(DataType.Time)]
         public TimeOnly BaslangicSaati { get; set; }
 
-        [Required]
-        [Display(Name = "Duruş Sebebi")]
         public string Sebep { get; set; } = string.Empty;
 
 
         // Foreign Key
-        [Required]
-        [Display(Name = "Kaynak Döngüsü")]
         public int KaynakDongusuId { get; set; }
 
         // Navigation Property
