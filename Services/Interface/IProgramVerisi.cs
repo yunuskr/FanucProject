2ce65f09b6d281944e68498ab5f574f98fd56c4c
Ahmettcakar <116648416+Ahmettcakar@users.noreply.github.com>
2026-02-20 09:46:36 +0300

    güncelleme

--- Services/Interface/IProgramVerisi.cs
+++ Services/Interface/IProgramVerisi.cs
@@ -1,12 +1,11 @@
 using FanucRelease.Models;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+
 
 namespace FanucRelease.Services.Interfaces
 {
     public interface IProgramVerisiService:IGenericService<ProgramVerisi>
     {
-        Task<List<FanucRelease.Models.ViewModels.GecmisProgramRow>> GetGecmisProgramlarRowsAsync(int take = 100);
+        Task<List<Models.ViewModels.GecmisProgramRow>> GetGecmisProgramlarRowsAsync(int take = 100);
         Task<ProgramVerisi?> GetLastProgramAsync();
         Task<int> GetTotalProgramCountAsync();
         Task<(int thisWeek, int lastWeek)> GetProgramCountsForThisAndLastWeekAsync();
